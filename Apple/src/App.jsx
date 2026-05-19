fb45b1dc67cbb3a1d7e88c291d3cdaa9cf3f75e0
Abrham Biresaw <abrhambiresaw2112@gmail.com>
2026-05-19 14:07:04 +0300

    import MacBook to App.jsx

--- Apple/src/App.jsx
+++ Apple/src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 
 import Header from './components/Header/Header.jsx';
 import Footer from './components/Footer/footer.jsx';
+import MacBook from './components/macBook/macBook.jsx';
 
 function App() {
   return (
     <div>
       <Header />
 
+      <MacBook />
       <Footer />
     </div>
   )
