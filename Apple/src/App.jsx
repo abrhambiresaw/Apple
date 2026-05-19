ceba9d87aed5fa521dcf23bd3e113dc016e3ef52
Abrham Biresaw <abrhambiresaw2112@gmail.com>
2026-05-19 12:33:56 +0300

    convert footer html to react and import it in app.jsx

--- Apple/src/App.jsx
+++ Apple/src/App.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 
 import Header from './components/Header/Header.jsx';
+import Footer from './components/Footer/footer.jsx';
 
 function App() {
   return (
     <div>
       <Header />
+
+      <Footer />
     </div>
   )
 }
