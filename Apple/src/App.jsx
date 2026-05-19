a7b54a8ab5bd6bdec463c8e4366fa389582beb05
Abrham Biresaw <abrhambiresaw2112@gmail.com>
2026-05-19 14:21:44 +0300

    convert html to react and import it

--- Apple/src/App.jsx
+++ Apple/src/App.jsx
@@ -4,6 +4,7 @@ import Header from './components/Header/Header.jsx';
 import Footer from './components/Footer/footer.jsx';
 import MacBook from './components/macBook/macBook.jsx';
 import Iphone11 from './components/iphone11/Iphone11.jsx';
+import Iphone from './components/Iphone/Iphone.jsx';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
 
       <MacBook />
       <Iphone11 />
+      <Iphone />
       <Footer />
     </div>
   )
