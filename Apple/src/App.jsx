7fbf1932d90c0aaed0bddb3068fad2c348fc5cd4
Abrham Biresaw <abrhambiresaw2112@gmail.com>
2026-05-19 15:33:21 +0300

    update mispelled

--- Apple/src/App.jsx
+++ Apple/src/App.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
 import Header from './components/Header/Header.jsx';
-import Footer from './components/Footer/footer.jsx';
-import MacBook from './components/macBook/macBook.jsx';
-import Iphone11 from './components/iphone11/Iphone11.jsx';
+import Footer from './components/Footer/Footer.jsx';
+import MacBook from './components/MacBook/MacBook.jsx';
+import Iphone11 from './components/Iphone11/Iphone11.jsx';
 import Iphone from './components/Iphone/Iphone.jsx';
 import Watch from './components/Watch/Watch.jsx';
 import AirPods from './components/AirPods/AirPods.jsx';
