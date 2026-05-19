b8e63131aede981e0eedff0248f09a8e06009b44
Abrham Biresaw <abrhambiresaw2112@gmail.com>
2026-05-19 14:28:32 +0300

    create folder, convert html to react and import it

---
+++ Apple/src/components/Watch&card/Watch.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+
+function Watch() {
+  return (
+    <div>
+
+       <section className="fourth-heghlight-wrapper">
+      <div className="left-side-wrapper">
+        <div className="top-logo-wrapper">
+          <div className="logo-wrapper">
+            <img src="src/components/images/watch-series-5.jpg" />
+          </div>
+        </div>
+        <div className="description-wraper">
+          With the new Always-On Retina display.<br />
+          You’ve never seen a watch like this.
+        </div>
+        <div className="links-wrapper">
+          <ul>
+            <li><a href="">Learn more</a></li>
+            <li><a href="">Buy</a></li>
+          </ul>
+        </div>
+      </div>
+      <div className="right-side-wrapper">
+        <div className="top-logo-wrapper">
+          <div className="logo-wrapper">
+            <img src="src/components/images/apple-card.jpg" />
+          </div>
+        </div>
+        <div className="description-wraper">
+          Get 3% Daily Cash on purchases from Apple using Apple Card.
+        </div>
+        <div className="links-wrapper">
+          <ul>
+            <li><a href="">Learn more</a></li>
+            <li><a href="">Buy</a></li>
+          </ul>
+        </div>
+      </div>
+    </section>
+
+    </div>
+  )
+}
+
+export default Watch
