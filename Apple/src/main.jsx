ab8bf69596be33b04b1bb47ee1d96478b183c227
Abrham Biresaw <abrhambiresaw2112@gmail.com>
2026-05-27 14:30:34 +0300

    Add React Router setup with BrowserRouter

--- Apple/src/main.jsx
+++ Apple/src/main.jsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './components/css/style.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import {BrowserRouter} from "react-router";
+import "./components/css/style.css";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+
+      <App />
+
+    </BrowserRouter>
   </StrictMode>,
-)
+);
