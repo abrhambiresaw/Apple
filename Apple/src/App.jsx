deba3397f571e8b0d8a80f75d24f4eff40b430db
Abrham Biresaw <abrhambiresaw2112@gmail.com>
2026-05-27 14:35:51 +0300

    setup routing using Routes

--- Apple/src/App.jsx
+++ Apple/src/App.jsx
@@ -1,30 +1,33 @@
-import React from 'react';
+import React from "react";
 
-import Header from './components/Header/Header.jsx';
-import Footer from './components/Footer/Footer.jsx';
-import MacBook from './components/MacBook/MacBook.jsx';
-import Iphone11 from './components/Iphone11/Iphone11.jsx';
-import Iphone from './components/Iphone/Iphone.jsx';
-import Watch from './components/Watch/Watch.jsx';
-import AirPods from './components/AirPods/AirPods.jsx';
-import MacBookPro from './components/MacBookPro/MacBookPro.jsx';
-import YouTube from './components/YouTube/YouTube.jsx';
+import Header from "./components/Header/Header.jsx";
+import Footer from "./components/Footer/Footer.jsx";
+import MacBook from "./components/MacBook/MacBook.jsx";
+import Iphone11 from "./components/Iphone11/Iphone11.jsx";
+import Iphone from "./components/Iphone/Iphone.jsx";
+import Watch from "./components/Watch/Watch.jsx";
+import AirPods from "./components/AirPods/AirPods.jsx";
+import MacBookPro from "./components/MacBookPro/MacBookPro.jsx";
+import YouTube from "./components/YouTube/YouTube.jsx";
+import { Routes } from "react-router";
 
 function App() {
   return (
     <div>
       <Header />
+      <Routes>
+        <MacBook />
+        <Iphone11 />
+        <Iphone />
+        <Watch />
+        <AirPods />
+        <MacBookPro />
+        {/* <YouTube /> */}
+      </Routes>
 
-      <MacBook />
-      <Iphone11 />
-      <Iphone />
-      <Watch />
-      <AirPods />
-      <MacBookPro />
-      {/* <YouTube /> */}
       <Footer />
     </div>
-  )
+  );
 }
 
 export default App;
