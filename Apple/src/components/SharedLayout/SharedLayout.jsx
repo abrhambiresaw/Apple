ff139a904d2edf50f42cf635c889de725dd7f268
Abrham Biresaw <abrhambiresaw2112@gmail.com>
2026-05-28 11:12:31 +0300

    collect shared components in one file

---
+++ Apple/src/components/SharedLayout/SharedLayout.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from '../Header/Header'
+import Footer from '../Footer/Footer'
+
+function SharedLayout() {
+  return (
+    <>
+      <Header />
+      <Footer />
+    </>
+  )
+}
+
+export default SharedLayout
